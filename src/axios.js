4592c81ec941819d6be21b1b470de3d5fcf35b23
mateo-orozco <70611666+mateo-orozco@users.noreply.github.com>
2023-04-03 16:38:46 -0500

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -3,4 +3,4 @@ import axios from "axios";
 
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL =  "https://trialtasker-production.up.railway.app/" ;
+axios.defaults.baseURL =  "https://trialtasker.up.railway.app/" ;
